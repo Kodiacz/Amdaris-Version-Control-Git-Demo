fd816a3a9801d2427bc608498a7fc39ffd9af03c
Simeon Yordanov <lookmanche@gmail.com>
2022-11-08 17:37:43 +0200

    clear

--- VersionControlDemo/VersionControlDemo/SimeonFolder/Cat.cs
+++ VersionControlDemo/VersionControlDemo/SimeonFolder/Cat.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VersionControlDemo.SimeonFolder
+﻿namespace VersionControlDemo.SimeonFolder
 {
-    internal class Cat
+    public class Cat
     {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
     }
 }
